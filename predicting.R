5a31be22738a2fb0ecd0dbf374131945d2403d07
pedronovaes <pedronovaesmelo@gmail.com>
2021-05-11 00:31:40 -0300

    Information about predicting script

---
+++ predicting.R
@@ -0,0 +1,3 @@
+# In this code, we are going to to practice predictions based on gene expressions. This implies some data preprocessing then building
+# a classification model to predict a disease. Here we are continuing to work on a breast cancer dataset downloaded from Firehose
+# (https://gdac.broadinstitute.org/). These are the next generation sequencing data that are provided already normalized.
